69eda4a17c117ad63853fde4492a46005c1d8f1d
Chinseok Lee <allieuslee@gmail.com>
2016-01-27 15:11:18 +0900

    blog.views.comment_edit 뷰 구현

--- programming/blog/views.py
+++ programming/blog/views.py
@@ -64,10 +64,27 @@ def comment_new(request, post_pk):
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            form.save()
+            comment = form.save(commit=False)
+            comment.post = Post.objects.get(pk=post_pk)
+            comment.save()
             return redirect('blog.views.post_detail', post_pk)
     else:
         form = CommentForm()
     return render(request, 'blog/comment_form.html', {
         'form': form,
     })
+
+
+def comment_edit(request, post_pk, pk):
+    comment = Comment.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('blog.views.post_detail', post_pk)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/comment_form.html', {
+        'form': form,
+        })
