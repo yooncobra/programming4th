c1c211d694d0c288a4a7689b332f5b3bc31ec610
Chinseok Lee <allieuslee@gmail.com>
2016-02-03 15:54:37 +0900

    magazine.views.article_new 뷰 구현 (staff only)

--- publisher/magazine/urls.py
+++ publisher/magazine/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^articles/$', views.article_list, name='article_list'),
+    url(r'^articles/new/$', views.article_new, name='article_new'),
 ]
 
--- publisher/magazine/views.py
+++ publisher/magazine/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import redirect, render
 from .models import Article
+from .forms import ArticleForm
 
 
 def index(request):
@@ -10,5 +12,21 @@ def article_list(request):
     article_list = Article.objects.all()
     return render(request, 'magazine/article_list.html', {
         'article_list': article_list,
+    })
+
+
+@staff_member_required
+def article_new(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.save()
+            return redirect('magazine:article_list')
+    else:
+        form = ArticleForm()
+    return render(request, 'form.html', {
+        'form': form,
     })
 
