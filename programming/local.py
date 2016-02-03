0d3a8abf9295bf2ae1e0195f0ca9c57ca0dfab26
Chinseok Lee <allieuslee@gmail.com>
2016-02-03 14:41:10 +0900

    publisher 프로젝트 생성

--- programming/local.py
+++ programming/local.py
@@ -1,6 +1,6 @@
 EMAIL_HOST = 'smtp.naver.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'myaccount@naver.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'allieuslee@naver.com'
+EMAIL_HOST_PASSWORD = '1029shake'
 EMAIL_USE_TLS = True
 
