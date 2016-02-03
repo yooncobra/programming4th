2ce5b63a369ef494f269eb5efc81deaef68fb259
Chinseok Lee <allieuslee@gmail.com>
2016-02-03 15:44:44 +0900

    magazine.views.article_list 구현

--- publisher/magazine/urls.py
+++ publisher/magazine/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^articles/$', views.article_list, name='article_list'),
 ]
 
--- publisher/magazine/views.py
+++ publisher/magazine/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Article
 
 
 def index(request):
     return render(request, 'magazine/index.html')
+
+
+def article_list(request):
+    article_list = Article.objects.all()
+    return render(request, 'magazine/article_list.html', {
+        'article_list': article_list,
+    })
 
