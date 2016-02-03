ae96ad893ec1b9ef269bbc08ad00031e8d26592b
Chinseok Lee <allieuslee@gmail.com>
2016-02-03 15:28:15 +0900

    magazine 앱 내에 Category/Article/Comment 모델을 생성하고, admin 에 등록

--- publisher/magazine/admin.py
+++ publisher/magazine/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Article, Comment
 
-# Register your models here.
+
+admin.site.register(Category)
+
+admin.site.register(Article)
+
+admin.site.register(Comment)
+
--- publisher/magazine/models.py
+++ publisher/magazine/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    udpated_at = models.DateTimeField(auto_now=True)
+
+
+class Article(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    udpated_at = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    udpated_at = models.DateTimeField(auto_now=True)
+
