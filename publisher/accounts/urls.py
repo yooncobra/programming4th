4a2caf9c1ae07159877066af5d4b3b32ecf42bcb
Chinseok Lee <allieuslee@gmail.com>
2016-02-03 15:22:52 +0900

    accounts.views.profile 뷰 구현 (settings.LOGIN_REDIRECT_URL 에서 지정된 뷰)

--- publisher/accounts/urls.py
+++ publisher/accounts/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login
+from . import views
 
 
 urlpatterns = [
     url(r'^login/$', login, name='login', kwargs={
         'template_name': 'form.html',
     }),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
 
