32c26934dfed5c1f3a42ae907a549f3640a867b8
Chinseok Lee <allieuslee@gmail.com>
2016-02-04 16:23:35 +0900

    micro_django 프로젝트를 flask 스타일로 변형 (fun~ fun~)

--- micro_django.py
+++ micro_django.py
@@ -11,13 +11,29 @@ python micro_django.py runserver
 
 DEBUG = True
 SECRET_KEY = 'foo'
-
-urlpatterns = [
-    url(r'^$', lambda request: HttpResponse('hello world')),
-    url(r'^(?P<name>\w+)/$', lambda request, name: HttpResponse('hello, {}'.format(name))),
-]
-
 ROOT_URLCONF = __name__
+
+urlpatterns = []
+
+
+def route(pattern, *pattern_args, **pattern_kwargs):
+    def wrap_fn(view_fn):
+        urlpatterns.append(url(pattern, view_fn, *pattern_args, **pattern_kwargs))
+        def wrap(request, *args, **kwargs):
+            return view_fn(request, *args, **kwargs)
+        return wrap
+    return wrap_fn
+
+
+@route(r'^$')
+def index(request):
+    return HttpResponse('Hello World')
+
+
+@route(r'^(?P<name>\w+)/$')
+def hello(request, name):
+    return HttpResponse('Hello, {}'.format(name))
+
 
 if __name__ == '__main__':
     from django.core.management import execute_from_command_line
