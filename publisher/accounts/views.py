bf65ae3488652586f47e45c3899eac238cfa4462
Chinseok Lee <allieuslee@gmail.com>
2016-02-04 11:50:32 +0900

    회원가입과 동시에 로그인 수행

--- publisher/accounts/views.py
+++ publisher/accounts/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
@@ -9,9 +10,15 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, '회원 가입해주셔서 감사드려요. ;)')
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+
+            authenticated_user = authenticate(
+                    username=form.cleaned_data['username'],
+                    password=form.cleaned_data['password1'])
+            auth_login(request, authenticated_user)
+
+            messages.success(request, '환영합니다. ;)')
+            return redirect(settings.LOGIN_REDIRECT_URL)
     else:
         form = UserCreationForm()
     return render(request, 'form.html', {'form': form})
